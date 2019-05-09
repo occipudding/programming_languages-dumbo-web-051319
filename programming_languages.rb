9395fa0afc477663673c495b2112485d6e78d0e6
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 08:52:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,11 +36,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.keys.each do |lang_style|
     languages[lang_style].each do |lang, lang_type|
-      binding.pry
       new_hash[lang][:type] = lang_type
       new_hash[lang][:style].kind_of?(Array) ? new_hash[lang][:style] << lang_style : new_hash[lang][:style] = [lang_style]
     end
   end
+  binding.pry
 end
 
 reformat_languages(languages)
